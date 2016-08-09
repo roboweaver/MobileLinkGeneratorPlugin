e1f734e42db06bb5766db7b978f248de2a9add5b
Rob Weaver <rob.weaver@workfront.com>
2016-08-08 20:42:29 -0600

    Update code to get the right URL

--- omnis_link_rel.php
+++ omnis_link_rel.php
@@ -27,19 +27,23 @@
   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
+function getImageURL($fname){
+	 echo plugins_url( $fname ,__FILE__ ); 
+}
 function addTheIcons() {
+	$dir =  plugin_dir_path( __FILE__ );
 	?>
-	<link rel="shortcut icon" href="<?php echo plugins_url(); ?>/images/favicon.ico" type="image/x-icon" />
-	<link rel="icon" sizes="192x192" href="<?php echo plugins_url(); ?>/images/apple-touch-icon-192x192.png" type="image/png" />
-	<link rel="apple-touch-icon" href="<?php echo plugins_url(); ?>/images/apple-touch-icon.png" type="image/png" />
-	<link rel="apple-touch-icon" sizes="57x57" href="<?php echo plugins_url(); ?>/images/apple-touch-icon-57x57.png}" type="image/png" />
-	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo plugins_url(); ?>/images/apple-touch-icon-72x72.png" type="image/png" />
-	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo plugins_url(); ?>/images/apple-touch-icon-76x76.png" type="image/png" />
-	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo plugins_url(); ?>/images/apple-touch-icon-114x114.png" type="image/png" />
-	<link rel="apple-touch-icon" sizes="120x120" href="<?php echo plugins_url(); ?>/images/apple-touch-icon-120x120.png" type="image/png" />
-	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo plugins_url(); ?>/images/apple-touch-icon-144x144.png" type="image/png" />
-	<link rel="apple-touch-icon" sizes="152x152" href="<?php echo plugins_url(); ?>/images/apple-touch-icon-152x152.png" type="image/png" />
-	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo plugins_url(); ?>/images/apple-touch-icon-180x180.png" type="image/png" />
+<link rel="shortcut icon" href="<?php echo getImageURL( 'images/favicon.ico'); ?>" type="image/x-icon" />
+	<link rel="icon" sizes="192x192" href="<?php getImageURL('images/apple-touch-icon-192x192.png'); ?>" type="image/png" />
+	<link rel="apple-touch-icon" href="<?php getImageURL('images/apple-touch-icon.png'); ?>" type="image/png" />
+	<link rel="apple-touch-icon" sizes="57x57" href="<?php getImageURL('images/apple-touch-icon-57x57.png'); ?>" type="image/png" />
+	<link rel="apple-touch-icon" sizes="72x72" href="<?php getImageURL('images/apple-touch-icon-72x72.png'); ?>" type="image/png" />
+	<link rel="apple-touch-icon" sizes="76x76" href="<?php getImageURL('images/apple-touch-icon-76x76.png'); ?>" type="image/png" />
+	<link rel="apple-touch-icon" sizes="114x114" href="<?php getImageURL('images/apple-touch-icon-114x114.png'); ?>" type="image/png" />
+	<link rel="apple-touch-icon" sizes="120x120" href="<?php getImageURL('images/apple-touch-icon-120x120.png' ); ?>" type="image/png" />
+	<link rel="apple-touch-icon" sizes="144x144" href="<?php getImageURL('images/apple-touch-icon-144x144.png' ); ?>" type="image/png" />
+	<link rel="apple-touch-icon" sizes="152x152" href="<?php getImageURL('images/apple-touch-icon-152x152.png' ); ?>" type="image/png" />
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php getImageURL('images/apple-touch-icon-180x180.png' ); ?>" type="image/png" />
 	<?php
 }
 
